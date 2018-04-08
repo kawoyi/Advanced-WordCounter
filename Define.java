fd737671332190d14861be796f4bde33904e093c
thunder controller Tesla <382912076@qq.com>
2018-04-08 23:19:30 +0800

    Delete Define.java

--- Define.java
+++
@@ -1,7 +0,0 @@
-package profile;
-
-public class Define{
-    
-    public static char[] seperator={'\r','\n','\t'};
-    public static char[] validCharacter={'!','!','#','$','%','^','&','*','(',')','[',']','{','}','\'','"',';',':','<','>','?','/','+','-','|'};//³£ÓÃ×Ö·û¼¯
-}
